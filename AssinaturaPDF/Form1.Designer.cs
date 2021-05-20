7c0c91782c63b6913c86b199ccd7b21b6ca51c5e
rfl808 <73585766+rfl808@users.noreply.github.com>
2021-05-20 14:26:47 -0300

    Add files via upload

---
+++ AssinaturaPDF/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace AssinaturaPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(244, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 450);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clique aqui para escolher o arquivo PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(25, 236);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(191, 47);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            320,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ALTURA DA ASSINATURA";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 59);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Este programa adiciona um campo de assinatura digital em um arquivo PDF.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Assinatura PDF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AssinaturaPDF/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Forms;
+using iText.Forms.Fields;
+
+
+namespace AssinaturaPDF
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            using (OpenFileDialog openFile = new OpenFileDialog())
+            {
+                
+                openFile.Filter = "pdf files(*.pdf)|*.pdf";
+                openFile.Title = "Escolha o arquivo PDF para adicionar uma assinatura.";
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    
+                    string filePath = openFile.FileName;
+                    string outPath = RemoveExtension(filePath);
+                    AddSignature(filePath, outPath + "-Assinatura.pdf");                    
+                    MessageBox.Show("Arquivo com assinatura salvo em " + outPath + "-Assinatura.pdf",
+                                        "Arquivo Salvo",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Information);
+
+                }
+            }
+        }
+        private void AddSignature(string filePath, string outPath)
+        {
+            Random rand = new Random();
+            int rndNum = rand.Next();
+            decimal idec = this.numericUpDown1.Value;
+            float signatureHeight =(float)idec;
+            PdfReader reader = new PdfReader(filePath);
+            FileStream fs = new FileStream(outPath, FileMode.Create, FileAccess.Write);
+            PdfWriter writer = new PdfWriter(fs);
+            PdfDocument pdfDoc = new PdfDocument(reader, writer);            
+            PdfAcroForm form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+            Rectangle rect = new Rectangle(0, signatureHeight, 800, 50);            
+            PdfFormField signature = PdfFormField.CreateSignature(pdfDoc, rect);
+            signature.SetFieldName("Assinatura"+rndNum);
+            form.AddField(signature, pdfDoc.GetFirstPage());
+            pdfDoc.Close();
+
+        }
+
+        private string RemoveExtension(string filePath)
+        {
+            int lio = filePath.LastIndexOf(@".pdf");
+            string outPut = filePath.Substring(0, lio);
+            Console.WriteLine(outPut);
+            return outPut;
+        }
+               
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+    public static class StringExtensions
+    {
+        public static string Left(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            maxLength = Math.Abs(maxLength);
+
+            return (value.Length <= maxLength
+                   ? value
+                   : value.Substring(0, maxLength)
+                   );
+        }
+    }
+}
